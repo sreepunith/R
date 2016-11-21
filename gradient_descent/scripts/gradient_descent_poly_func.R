28e373e2685cbe0051ad509fb8e8220622373d9a
newbiettn <newbiettn@gmail.com>
2016-11-22 10:45:13 +1100

    add poly function

--- gradient_descent/scripts/gradient_descent_poly_func.R
+++ gradient_descent/scripts/gradient_descent_poly_func.R
@@ -1,55 +1,59 @@
 ################################################################
 # LIBRARY
 ################################################################
-library(ggplot2)
-source("multiplot.R")
+library(ggplot2) #graphs
+# library(polynom) #generate polynomial functions
+source("scripts/multiplot.R") #plot multiple at once
 
 ################################################################
 # GENERATE DATA
 ################################################################
 # x is a sequence from [-45; 50]
-x <- seq(-45, 50, by = 5)
+x <- seq(-5, 1, .05)
 
-# Define a quadratic function ax^2 + bx + c
-a <- 1
-b <- 2
-c <- 3
-quad_func <- function(x, a, b, c){
-  return (a*(x^2) + b*x + c);
+poly_func <- function(x){
+  return (-120*x - 154*x^2 + 49*x^3 + 140*x^4 + 70*x^5 + 14*x^6 + x^7 );
 }
 
-# Generate y values 
-y <- sapply(x, quad_func, a, b, c)
+# polynomial function
+# calculated_poly <- poly.calc(-5:1) #polynom
+# calculated_poly
+# poly_func <- as.function(calculated_poly)
+
+# Aggreate as a df
+y <- sapply(x, poly_func)
+df <- data.frame(x, y)
+# df <- data.frame(x, y = poly_func)
 
 # Plot the function
-df <- data.frame(x, y)
 ggplot(data = df, mapping = aes(x = x, y = y)) +
-  geom_line(linetype="dashed", colour = "red") +
+  stat_function(fun=poly_func, colour="red") +
   geom_point(shape=21, size=2, fill="blue", stroke = .1)
 
 ################################################################
 # GRADIENT DESCENT
 ################################################################
-m <- 5 # iterations
-eta <- c(0.1, 0.3, 0.5, 0.7) # learning rate
-x_init <- 45 # initial value
+m <- 10 # iterations
+eta <- c(0.01, 0.02, 0.005, 0.007) # learning rate
+x_init <- -4.5 # initial value
 
 # Gradient descent w.r.t. quadractic function ax^2+bx+c
+gradient <- function(x) {
+  return (-120 - 308*x + 147*x^2 + 560*x^3 + 350*x^4 + 84*x^5 + 7*x^6)
+}
 grad_desc <- function(x_init, eta, m) {
   # data frame to store the result
   result <- data.frame("id" = numeric(), "x" = numeric(), "y" = numeric())
-  y_init <- quad_func(x_init, a, b, c)
+  y_init <- poly_func(x_init)
   # store the initial point as a result
   result[1,] <- c(1, x_init, y_init)
   
   x <- x_init
   for(indx in 2:m) {
-    #gradient
-    grad <- 2*a*x+b
     #compute new x
-    x <- x - eta * grad
+    x <- x - eta * gradient(x)
     #compute new value of y by new x
-    y <- quad_func(x, a, b, c)
+    y <- poly_func(x)
     # update the result
     result[indx,] <- c(indx, x, y)
   }
@@ -65,35 +69,35 @@ grad_result_3 <- grad_desc(x_init, eta[2], m)
 grad_result_5 <- grad_desc(x_init, eta[3], m)
 grad_result_7 <- grad_desc(x_init, eta[4], m)
 
-p1 <- ggplot() +
-  geom_line(data = df, mapping = aes(x = x, y = y), linetype="dashed", colour = "red") +
+p1 <- ggplot(data = df, mapping = aes(x = x, y = y)) +
+  stat_function(data = df, fun=poly_func, colour="red") +
   geom_path(data = data.frame(grad_result_1), aes(x = x, y = y), colour = "blue") +
   geom_point(data = data.frame(grad_result_1), aes(x = x, y = y)) +
-  geom_text(data = data.frame(grad_result_1), aes(x = x, y = y, label = id), vjust=-1, colour = "blue") +
-  ggtitle(expression(paste(eta, " = 0.1")))
+  ggtitle(expression(paste(eta, " = 0.01"))) +
+  coord_cartesian(xlim = c(-5:1), ylim = c(-100, 100))
 
-p2 <- ggplot() +
-  geom_line(data = df, mapping = aes(x = x, y = y), linetype="dashed", colour = "red") +
+p2 <- ggplot(data = df, mapping = aes(x = x, y = y)) +
+  stat_function(data = df, fun=poly_func, colour="red", geom = "line") +
   geom_path(data = data.frame(grad_result_3), aes(x = x, y = y), colour = "blue") +
   geom_point(data = data.frame(grad_result_3), aes(x = x, y = y)) +
-  geom_text(data = data.frame(grad_result_3), aes(x = x, y = y, label = id), vjust=-1, colour = "blue") +
-  ggtitle(expression(paste(eta, " = 0.3")))
+  ggtitle(expression(paste(eta, " = 0.02"))) +
+  coord_cartesian(xlim = c(-5:1), ylim = c(-100, 100))
 
-p3 <- ggplot() +
-  geom_line(data = df, mapping = aes(x = x, y = y), linetype="dashed", colour = "red") +
+p3 <- ggplot(data = df, mapping = aes(x = x, y = y)) +
+  stat_function(data = df, fun=poly_func, colour="red") +
   geom_path(data = data.frame(grad_result_5), aes(x = x, y = y), colour = "blue") +
   geom_point(data = data.frame(grad_result_5), aes(x = x, y = y)) +
-  geom_text(data = data.frame(grad_result_5), aes(x = x, y = y, label = id), vjust=-1, colour = "blue") +
-  ggtitle(expression(paste(eta, " = 0.5")))
+  ggtitle(expression(paste(eta, " = 0.005"))) +
+  coord_cartesian(xlim = c(-5:1), ylim = c(-100, 100))
 
-p4 <- ggplot() +
-  geom_line(data = df, mapping = aes(x = x, y = y), linetype="dashed", colour = "red") +
+p4 <- ggplot(data = df, mapping = aes(x = x, y = y)) +
+  stat_function(data = df, fun=poly_func, colour="red") +
   geom_path(data = data.frame(grad_result_7), aes(x = x, y = y), colour = "blue") +
   geom_point(data = data.frame(grad_result_7), aes(x = x, y = y)) +
-  geom_text(data = data.frame(grad_result_7), aes(x = x, y = y, label = id), vjust=-1, colour = "blue") +
-  ggtitle(expression(paste(eta, " = 0.7")))
+  ggtitle(expression(paste(eta, " = 0.007"))) +
+  coord_cartesian(xlim = c(-5:1), ylim = c(-100, 100))
 
 # Use extra function for multiple plot
-multiplot(p1, p3, p2, p4, cols=2)
-dev.copy(png,filename="figs/gradient_desc_quad_func.png")
+multiplot(p1, p3, p2, p4, cols = 2)
+dev.copy(png,filename="figs/gradient_desc_poly_func.png")
 dev.off ()
