33cad235e0174898c1a0c9dc579ce57885acebbe
newbiettn <newbiettn@gmail.com>
2017-05-15 16:21:08 +1000

    update file names and remove swedish example

--- bayes/bayes_decision_boundary_2.R
+++ bayes/bayes_decision_boundary_2.R
@@ -11,8 +11,8 @@ set.seed(123456)
 sigma_1 <- matrix(c(1/2, 0, 0, 2), nrow = 2, ncol = 2, byrow = 2)
 sigma_2 <- matrix(c(2, 0, 0, 2), nrow = 2, ncol = 2, byrow = 2)
 
-mu_sample_0 <- mvrnorm(n = 1000, mu = c(3, 6), Sigma = sigma_1) #class 0
-mu_sample_1 <- mvrnorm(n = 1000, mu = c(3, -2), Sigma = sigma_2) #class 1 
+mu_sample_0 <- mvrnorm(n = 10, mu = c(3, 6), Sigma = sigma_1) #class 0
+mu_sample_1 <- mvrnorm(n = 10, mu = c(3, -2), Sigma = sigma_2) #class 1 
 
 dt <- rbind(mu_sample_0, mu_sample_1) 
 dt <- as.data.frame(dt)
@@ -24,19 +24,3 @@ dt$class <- as.factor(dt$class)
 # Point plot
 ggplot() +
   geom_point(dt, mapping = aes(x = x, y = y, colour = class))
-
-# Decision boundary
-dt.lda <- lda(class ~ ., dt, prior = c(0.5, 0.5))
-
-np <- 300
-nd.x <- seq(from = min(dt$x), to = max(dt$x), length.out = np)
-nd.y <- seq(from = min(dt$y), to = max(dt$y), length.out = np)
-nd <- expand.grid(x = nd.x, y = nd.y)
-prd <- as.numeric(predict(dt.lda, newdata = nd)$class)
-
-dt_contour <- data.frame(x = nd$x, y = nd$y, z = prd)
-
-ggplot() +
-  geom_point(dt, mapping = aes(x = x, y = y, colour = class)) +
-  stat_contour(dt_contour, mapping = aes(x = x, y = y, z = z))
-
